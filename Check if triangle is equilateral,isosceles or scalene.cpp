bd471c2c2ffd8ecc7801136c78fb2bc8a0d3f3d1
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-05 10:00:29 +0530

    Update Check if triangle is equilateral,isosceles or scalene.cpp

--- Check if triangle is equilateral,isosceles or scalene.cpp
+++ Check if triangle is equilateral,isosceles or scalene.cpp
@@ -1,1 +1,23 @@
+# include<iostream>
+using namespace std;
 
+int main()
+{
+    int a,b,c;
+    cout<<"Input three sides of a triangle";
+    cin>>a>>b>>c;
+    
+    if(a==b&&b==c)
+    {
+        cout<<"Equilateral Triangle";
+    }
+    else if(a==b || b==c || c==a)
+    {
+        cout<<"Isosceles Triangle";
+    }
+    else
+    {
+       cout<<"Scalene Triangle"; 
+    }
+    return 0;
+}

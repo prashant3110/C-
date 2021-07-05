e4f5155a55bc660a225f770eb5fe6f03e964ebb5
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-05 09:37:04 +0530

    Update Find MAX. , MIN. among two numbers.cpp

--- Find MAX. , MIN. among two numbers.cpp
+++ Find MAX. , MIN. among two numbers.cpp
@@ -1,1 +1,24 @@
-
+# include<iostream>
+using namespace std;
+ int main()
+ {
+     int n1,n2;
+     cin>>n1>>n2;
+     int MAX,MIN;
+     
+     if (n1>n2)
+     {
+         MAX=n1;
+         MIN=n2;
+     }
+     else
+     {
+         MAX=n2;
+         MIN=n1;
+     }
+     
+     cout<<"Max. = "<<MAX<<endl;
+     cout<<"Min. = "<<MIN;
+    return 0; 
+ }
+ 

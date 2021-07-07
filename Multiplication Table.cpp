67644bb174e88752e96d10adeaedf46594e23a1f
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-07 10:15:21 +0530

    Update Multiplication Table.cpp

--- Multiplication Table.cpp
+++ Multiplication Table.cpp
@@ -1,1 +1,17 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter positive Integer:";
+    cin>>a;
+    
+    for(int i=1;i<=10;i++)
+    {
+        cout<<a<<" * "<<i<<" = "<<a*i<<endl;
+    }
+
+    return 0;
+}
 

5702b1ae7a4fb989d3384237c9623faf0513b2ba
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-11 21:22:40 +0530

    Update Check whether given number is prime or not.cpp

--- Check whether given number is prime or not.cpp
+++ Check whether given number is prime or not.cpp
@@ -1,1 +1,23 @@
+# include<iostream>
+using namespace std;
 
+int main()
+{
+    int n,i;
+    cout<<"Enter number : ";
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"Non Prime";
+            break;
+        }
+    }
+    if(i==n)
+    {
+        cout<<"Prime";
+    }
+    return 0;
+    
+}

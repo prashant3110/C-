da762db2fc7a901165f77bec80f762803076b215
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-11 21:31:53 +0530

    Update Print all Prime numbers in a given range a and b.cpp

--- Print all Prime numbers in a given range a and b.cpp
+++ Print all Prime numbers in a given range a and b.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{     int  a,b,i,num;  
+      cout<<"Enter a,b : ";
+      cin>>a>>b;
+      
+      for(num=a;num<=b;num++)
+      {
+          for(i=2;i<num;i++)
+          {
+              if(num%i==0)
+              {
+                  break;
+              }
+          }
+         if(i==num)
+         {
+             cout<<num<<endl;
+         }
+      }
+      
+    return 0;  
+}

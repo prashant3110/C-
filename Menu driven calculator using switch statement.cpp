105daca460fde76c8bda7042135b5370f9d5292d
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-16 15:55:53 +0530

    Create Menu driven calculator using switch statement.cpp

---
+++ Menu driven calculator using switch statement.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cout<<"Enter Two numbers: ";
+    cin>>n1>>n2;
+    char ch;
+    cout<<"Enter operator(+ - * / %) : ";
+    cin>>ch;
+    
+    switch(ch)
+    {
+        case '+': cout<<n1+n2;
+        break;
+        
+        case '-': cout<<n1-n2;
+        break;
+        
+        case '*': cout<<n1*n2;
+        break;
+        
+        case '/': cout<<n1/n2;
+        break;
+        
+        case '%': cout<<n1%n2;
+        break;
+        
+        default: cout<<" Wrong Operator";
+        
+    }
+    
+    return 0;
+}

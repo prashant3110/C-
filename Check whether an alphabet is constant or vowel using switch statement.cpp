a79999e667a0b21048c13af9eec6d44665a9faba
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-16 16:50:03 +0530

    Update Check whether an alphabet is constant or vowel using switch statement.cpp

--- Check whether an alphabet is constant or vowel using switch statement.cpp
+++ Check whether an alphabet is constant or vowel using switch statement.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    char ch;
+    cout<<"Enter character : ";
+    cin>>ch;
+    
+    switch(ch)
+    {
+        case 'a' : cout<<"It is vowel";
+        break;
+        case 'e' : cout<<"It is vowel";
+        break;
+        case 'i' : cout<<"It is vowel";
+        break;
+        case 'o' : cout<<"It is vowel";
+        break;
+        case 'u' : cout<<"It is vowel";
+        break;
+        case 'A' : cout<<"It is vowel";
+        break;
+        case 'E' : cout<<"It is vowel";
+        break;
+        case 'I' : cout<<"It is vowel";
+        break;
+        case 'O' : cout<<"It is vowel";
+        break;
+        case 'U' : cout<<"It is vowel";
+        break;
+        default: cout<<"It is a constant";
+    }
+    
+    return 0;
+}

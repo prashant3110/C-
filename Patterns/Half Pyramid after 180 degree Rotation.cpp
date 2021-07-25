2397bbad095a91d19f79afb4690faac61961467a
Prashant <prashantchauhan3110@gmail.com>
2021-07-25 18:12:15 +0530

    HP after 180 added

---
+++ Patterns/Half Pyramid after 180 degree Rotation.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+using namespace std;
+
+int main()
+{
+    int  n;
+    cout<<"Enter n : ";
+    cin>>n;
+    
+    for(int i=1;i<=n;i++)
+    {
+        int j;
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        
+        for( ;j<=n;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

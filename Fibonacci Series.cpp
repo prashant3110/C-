809e0839a0280c3724efca11e37b9bbdd5107bd9
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-27 12:29:07 +0530

    Update Fibonacci Series.cpp

--- Fibonacci Series.cpp
+++ Fibonacci Series.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n,nextTerm=0;
+    cout<<"Enter n: ";
+    cin>>n;
+    
+    int t1=0;
+    int t2=1;
+    
+    for(int i=1;i<=n;i++)
+    {
+        cout<<t1<<" ";
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+        
+    }
+    
+    return 0;
+}

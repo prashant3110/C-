a564a548d3bfb53c50bdc0c0903211cad6be8506
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-27 12:33:32 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fact(int num)
+{
+    int factorial=1;
+    for(int i=2;i<=num;i++)
+    {
+        factorial*=i;
+    }
+    
+    return factorial;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter number :";
+    cin>>n;
+    
+    int ans=fact(n);
+    cout<<ans;
+    
+    return 0;
+}
 

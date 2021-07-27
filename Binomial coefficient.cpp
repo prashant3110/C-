20925d040bd493875ae5f304971e2a54e93e6738
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-27 12:39:56 +0530

    Update Binomial coefficient.cpp

--- Binomial coefficient.cpp
+++ Binomial coefficient.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int fact(int num)
+{
+    int factorial=1;
+    for(int i=2;i<=num;i++)
+    {
+        factorial*=i;
+    }
+    
+    return factorial;
+}
+
+int main()
+{
+    int n,r;
+    cout<<"Enter the value of n and r :";
+    cin>>n>>r;
+    
+    int ans = fact(n)/(fact(r)*fact(n-r));
+    cout<<ans;
+    return 0;
+}
 

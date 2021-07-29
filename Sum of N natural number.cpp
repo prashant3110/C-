94a1b48bfc8f6bfbd5d004d9326269cb22fdb7ef
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-29 19:25:20 +0530

    Update Sum of N natural number.cpp

--- Sum of N natural number.cpp
+++ Sum of N natural number.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int sum(int num)
+{
+    int a=0;
+    for(int i=1;i<=num;i++)
+    {
+        a+=i;
+    }
+    
+    return a;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    int ans=sum(n);
+    cout<<"Result : "<<ans;
+    return 0;
+}

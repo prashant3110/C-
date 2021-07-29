b4216608909410c592d827958828f043c5221524
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-29 18:31:30 +0530

    Update Check whether given triplet is Pythagorean or not.cpp

--- Check whether given triplet is Pythagorean or not.cpp
+++ Check whether given triplet is Pythagorean or not.cpp
@@ -1,1 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+   bool check (int x, int y, int z)
+    {
+        int a,b,c;
+        a=max(x,max(y,z));
+        
+        if(a==x)
+        {
+            b=y;
+            c=z;
+        }
+        else if(a==y)
+        {
+            b=x;
+            c=z;
+        }
+        else
+        {
+            b=x;
+            c=y;
+        }
+        
+        
+        if(a*a==b*b+c*c)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+    }
+    
+     
+    
+    int main()
+{
+    int x,y,z;
+    cout<<"Enter three nuumbers:";
+    cin>>x>>y>>z;
+    
+    if(check(x,y,z))
+    {
+        cout<<"Pythagorean Triplet";
+    }
+    else
+    {
+        cout<<"Not Pythagorean Triplet";
+    }
+
+    return 0;
+} 
+    

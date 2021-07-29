c0687be69053946c445cd693a8ed3c933ca8997d
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-29 19:17:28 +0530

    Update Hexadecimal to Decimal.cpp

--- Hexadecimal to Decimal.cpp
+++ Hexadecimal to Decimal.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int hexadecimalTOdecimal(string s)
+{
+    int ans=0;
+    int x=1;
+    int len=s.size();
+    
+    for(int i=len-1;i>=0;i--)
+    {
+        if(s[i]>='0'&&s[i]<='9')
+        {
+            ans+=(s[i]-'0')*x;
+        }
+        else if(s[i]>='A'&&s[i]<='F')
+        {
+            ans+=(s[i]-'A'+10)*x;
+        }
+        
+        x=x*16;
+    }
+    
+    return ans;
+}
+
+
+int main()
+{
+    string s;
+    cout<<"Enter Hexadecimal number:";
+    cin>>s;
+    int a = hexadecimalTOdecimal(s);
+    cout<<a;
+    return 0;
+}

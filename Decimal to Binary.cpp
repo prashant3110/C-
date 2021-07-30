b9618bef89df09749c10a822f0d2768124bb8f28
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-30 19:40:29 +0530

    Update Decimal to Binary.cpp

--- Decimal to Binary.cpp
+++ Decimal to Binary.cpp
@@ -1,1 +1,28 @@
+# include<iostream>
+using namespace std;
 
+int decimaltobinary(int num)
+{
+    int ans=0;
+    int x=1;
+    
+    while(num>0)
+    {
+        int rem;
+        rem=num%2;
+        num=num/2;
+        ans=ans+rem*x;
+        x=x*10;
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter decimal number : ";
+    cin>>n;
+    int a=decimaltobinary(n);
+    cout<<a;
+}

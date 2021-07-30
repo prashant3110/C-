0052390b366515927287893fe123e2084f2c7ff3
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-30 19:43:37 +0530

    Update Decimal to Octal.cpp

--- Decimal to Octal.cpp
+++ Decimal to Octal.cpp
@@ -1,1 +1,28 @@
+# include<iostream>
+using namespace std;
 
+int decimaltoOctal(int num)
+{
+    int ans=0;
+    int x=1;
+    
+    while(num>0)
+    {
+        int rem;
+        rem=num%8;
+        num=num/8;
+        ans=ans+rem*x;
+        x=x*10;
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter decimal number : ";
+    cin>>n;
+    int a=decimaltoOctal(n);
+    cout<<a;
+}

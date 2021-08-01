47cbb456cec35a88fdcc5e40ee8d533cf0443793
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-01 18:58:11 +0530

    Update Find max. and min. number in an array.cpp

--- Find max. and min. number in an array.cpp
+++ Find max. and min. number in an array.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    
+    int arr[n];
+    
+    cout<<"Enter elements : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        
+    }
+    
+    int maxno=INT_MIN;
+    int minno=INT_MAX;
+    
+    for(int i=0;i<n;i++)
+    {
+        maxno=max(maxno,arr[i]);
+        minno=min(minno,arr[i]);
+    }
+    
+    cout<<"Max. : "<<maxno<<endl;
+    cout<<"Min. : "<<minno;
+    
+    return 0;
+}

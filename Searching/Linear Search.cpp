7262c38824142ea70717523c0ac632dfc81f95ee
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-01 19:19:13 +0530

    Update Linear Search.cpp

--- Searching/Linear Search.cpp
+++ Searching/Linear Search.cpp
@@ -1,1 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearsearch(int a[],int n,int key)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            return i;
+        }
+      
+    }
+     return -1;
+        
+    
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter elements :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int k;
+    cout<<"Enter Key element :";
+    cin>>k;
+    
+    cout<<linearsearch(arr,n,k);
+
+    
+    return 0;
+}
 

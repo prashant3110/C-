32c9647bc342229bb1a2fc5b846ebb0dcc1f5257
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-02 20:03:55 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int a[n];
+    
+    cout<<"Enter elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+    int counter=1;
+    
+    while(counter<n)
+    {
+        for(int i=0;i<n-counter;i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                int temp=a[i+1];
+                a[i+1]=a[i];
+                a[i]=temp;
+            }
+        }
+        
+        counter++;
+    }
+    
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    return 0;
+}

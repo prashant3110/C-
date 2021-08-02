eb84a864123614b46eefd2feb527c439a559632c
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-02 19:55:13 +0530

    Update Selection Sort.cpp

--- Sorting/Selection Sort.cpp
+++ Sorting/Selection Sort.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int a[n];
+    
+    cout<<"Enter elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    return 0;
+}

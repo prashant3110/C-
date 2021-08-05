466baf89d8f95c52d75eb873aa37d8301a2846ee
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-05 20:32:10 +0530

    Update Max till ith element.cpp

--- Max till ith element.cpp
+++ Max till ith element.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter n :";
+    int n;
+    cin>>n;
+    cout<<"Enter elements :";
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    int m=INT_MIN;
+    
+    for(int i=0;i<n;i++)
+    {
+        m=max(m,a[i]);
+        cout<<m<<" ";
+    }
+
+    return 0;
+    
+   
+}
 

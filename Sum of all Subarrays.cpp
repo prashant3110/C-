47233a78f6ae049bc4a3420162524262ffa69609
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-05 20:08:06 +0530

    Update Sum of all Subarrays.cpp

--- Sum of all Subarrays.cpp
+++ Sum of all Subarrays.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    cout<<"Enter elements : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    
+    int sum=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        sum=0;
+        for(int j=i;j<n;j++)
+        {
+            sum+=a[j];
+            cout<<sum<<" ";
+        }
+    }
+    
+   return 0; 
+}    

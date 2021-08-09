65118e2754eedb867594e502739ff6152b361ff6
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-09 21:50:42 +0530

    second commit

--- Print all possible Subarrays.cpp
+++ Print all possible Subarrays.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    cout<<"Enter elements :";
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            for(int k=i;k<=j;k++)
+            {
+                cout<<a[k]<<",";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}

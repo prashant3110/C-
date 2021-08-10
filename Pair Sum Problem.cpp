346f37d2b67b372561f19a0886405bdfb0cbe22a
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-10 12:38:21 +0530

    Update Pair Sum Problem.cpp

--- Pair Sum Problem.cpp
+++ Pair Sum Problem.cpp
@@ -1,1 +1,39 @@
+// Solution 1 
+//time complexity : O(n)
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int a[n];
+    cout<<"Enter elements :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    
+    int k;
+    cout<<"Enter k :";
+    cin>>k;
+    
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]+a[j]==k)
+            {
+                cout<<"index:"<<i<<" "<<j<<endl;
+            }
+            
+        }
+    }
+    
+
+return 0;   
+}

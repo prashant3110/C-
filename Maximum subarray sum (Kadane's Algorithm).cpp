7b893d3952a176cbe9bb7af6025a8ba4f5d8e4bc
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-10 11:45:16 +0530

    Update Maximum subarray sum (Kadane's Algorithm).cpp

--- Maximum subarray sum (Kadane's Algorithm).cpp
+++ Maximum subarray sum (Kadane's Algorithm).cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int a[n];
+    cout<<"Enter Elements : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    
+    int cursum=0;
+    int maxsum=INT_MIN;
+    
+    for(int i=0;i<n;i++)
+    {
+        cursum+=a[i];
+        if(cursum<0)
+        {
+            cursum=0;
+        }
+        
+        maxsum=max(maxsum,cursum);
+        
+        
+    }
+    
+    cout<<maxsum;
+    return 0;
+}

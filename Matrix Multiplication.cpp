1b8d5632e92a8db38961e77eb2d71e0d773c1b04
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-16 20:52:02 +0530

    Update Matrix Multiplication.cpp

--- Matrix Multiplication.cpp
+++ Matrix Multiplication.cpp
@@ -1,1 +1,66 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int m1,n1,m2,n2;
+    cout<<"Enter rows and columns of 1st matrix:";
+    cin>>m1>>n1;
+    int a[m1][n1];
+    cout<<"Enter elements :";
+    for(int i=0;i<m1;i++)
+    {
+        for(int j=0;j<n1;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    
+    cout<<"Enter rows and columns of 2nd matrix:";
+    cin>>m2>>n2;
+    int b[m2][n2];
+    if(n1==m2)
+    {
+        cout<<"Enter elements:";
+        for(int i=0;i<m2;i++)
+        {
+            for(int j=0;j<n2;j++)
+            {
+                cin>>b[i][j];
+            }
+        }
+        
+    }
+    else
+    {
+        cout<<"Wrong order entered!!!!"<<endl;
+        cout<<"Matrix multiplication not possible!!!!";
+    }
+    
+    int c[m1][n2];
+    
+    
+    for(int i=0;i<m1;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+            c[i][j]=0;
+            for(int k=0;k<n1;k++)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+    
+     for(int i=0;i<m1;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+            cout<<c[i][j]<<" ";
+        } 
+        cout<<endl;
+    }
+    
+    return 0;
+    
+}    

61442c72a75a125664cd5ed794f6ddc569503a60
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-21 10:46:22 +0530

    Update To Check Given word is Palindrome or not.cpp

--- To Check Given word is Palindrome or not.cpp
+++ To Check Given word is Palindrome or not.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    
+    bool check=1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]!=arr[n-1 - i])
+        {
+            check=0;
+            break;
+        }
+    }
+    
+    if(check==true)
+    {
+        cout<<"Palindrome";
+    }
+    else
+    {
+        cout<<"Not Palindrome";
+    }
+    
+    return 0;
+    
+}

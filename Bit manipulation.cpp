b393f4d032a228582e81dc48ec300c81c281a83a
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-27 18:25:58 +0530

    Update Bit manipulation.cpp

--- Bit manipulation.cpp
+++ Bit manipulation.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int getbit(int n, int pos)
+{
+    return ((n & (1<<pos)) !=0 );
+}
+
+int setbit(int n, int pos)
+{
+    return (n | (1<<pos));
+}
+
+int clearbit(int n, int pos)
+{
+    int mask = ~(1<<pos);
+    return (n & mask);
+}
+
+int updatebit(int n,int pos,int value)
+{
+    int mask = ~(1<<pos);
+    n= n & mask;
+    return(n | (value<<pos));
+}
+
+
+
+int main()
+{
+    int n=5;
+    int pos=2;
+    cout<<getbit(n,pos)<<endl;
+     
+    return 0;
+}

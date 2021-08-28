c4f5028aff2e50cb7f8d9525dd53b54cc3dc99ec
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-28 10:21:15 +0530

    Update Program to check if a given number is power of 2.cpp

--- Program to check if a given number is power of 2.cpp
+++ Program to check if a given number is power of 2.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+bool check(int n)
+{
+    return (n && !(n & (n-1)));
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    cout<<check(n);
+    return 0;
+}

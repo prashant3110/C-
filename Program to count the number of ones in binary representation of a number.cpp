5ad2d52d00ce0c5aa0b4f02037bf3e1f4e2e3a14
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-28 10:35:31 +0530

    Update Program to count the number of ones in binary representation of a number.cpp

--- Program to count the number of ones in binary representation of a number.cpp
+++ Program to count the number of ones in binary representation of a number.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int numberofones(int n)
+{
+    int count = 0;
+    while(n!=0)
+    {
+        n=n & (n-1);
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    cout<<numberofones(n);
+    return 0;
+}
